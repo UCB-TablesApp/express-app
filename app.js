1eb5638ba01bcd9d7eb8c7844ad004d9c460e953
Joshua Moore <jommoore2003@me.com>
2018-12-15 13:32:57 -0800

    Added script to makeReservation.html file

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var app = express();
 var PORT = 7000;
 
 
-array to store waitlist
+//array to store waitlist
 
-array to store reservations
+//array to store reservations
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -21,27 +21,13 @@ app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/viewTables.html"));
 });
 
-<<<<<<< HEAD
-=======
-app.get('/api/waitlist' function(req, res) {
-    // array of waitlist;
-});
-
-
-// get call for waitlist
-
-// get call for reservation
-
-//post call to add reservations/waitlist
-
->>>>>>> 0411d38ee73271cd0f094b912e835489fdcb2df8
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
 
 //Display all Reservations
 
-app.get("/api/reservation", function(req, res) {
+app.post("/api/reservation", function(req, res) {
   // return res.json(newReservation.name);
-  console.log(req.body.name);
+  console.log(req.body);
 });
