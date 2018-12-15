0411d38ee73271cd0f094b912e835489fdcb2df8
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2018-12-15 12:58:43 -0800

    routes for reservations & waitlist

--- app.js
+++ app.js
@@ -5,18 +5,32 @@ var app = express();
 var PORT = 7000;
 
 
+array to store waitlist
+
+array to store reservations
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/", function(req, res) {
+app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/makeReservation.html"));
 });
 
-app.get("/", function(req, res) {
+app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/viewTables.html"));
 });
 
+app.get('/api/waitlist' function(req, res) {
+    // array of waitlist;
+});
+
+
+// get call for waitlist
+
+// get call for reservation
+
+//post call to add reservations/waitlist
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
