09887b3b8b38822cfac7c695a1c8728d7759b5a9
Joshua Moore <jommoore2003@me.com>
2018-12-15 13:11:54 -0800

    Updated makeReservation.html with JavaScript

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/viewTables.html"));
 });
 
-
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
+});
+
+//Display all Reservations
+
+app.get("/api/reservation", function(req, res) {
+  // return res.json(newReservation.name);
+  console.log(req.body.name);
 });
